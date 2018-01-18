3d23bc0c146fb54d074a19aa2b515669d12489e7
pjvalentini <pj.valentini@gmail.com>
2018-01-17 20:44:52 -0500

    adding buttons next and prev functionality

--- client/public/js/home.js
+++ client/public/js/home.js
@@ -1,1 +1,28 @@
+// ajax get '/api/scrape' then (res) $('#team-info').append(res[0])
 
+/* global $ */
+$(document).ready(function() {
+	$.ajax({
+		method: 'GET',
+		url: '/api/scrape',
+	}).then((res) => {
+		// console.log(res);
+		var teamName = $("<h2>", {
+			text: res[0].team,
+		});
+		console.log(res[0].team);
+		var pointsPG = $("<h2>", {
+			text: res[0].ppg,
+		});
+		console.log(res[0].ppg);
+		$("#team-info").append(teamName).append(pointsPG);
+
+		$("#next-button").click(function() {
+			 console.log("next clicked!");
+
+		$("#prev-button").click(function() {
+			console.log("prev clicked!");
+		});
+		});
+	});
+});
--- server/controller/routes.js
+++ server/controller/routes.js
@@ -8,7 +8,7 @@ models.sequelize.sync();
 
 var router = express.Router();
 
-router.get('/', function(req,res) {
+router.get('/', function(req, res) {
 	models.Stat.findAll().then((stats) => {
 		// console.log(stats);
 		var teamStats = [];
@@ -16,44 +16,48 @@ router.get('/', function(req,res) {
 			teamStats.push(stat);
 			// console.log(teamStats);
 		});
-    // or
+		// res.json(teamStats); // returns 30 teams and ppg json object.
     res.render('home', { stats: teamStats });
+		// or
 		// res.render('home', { stats: stats });
 	});
 });
 
-// run through postmant to populate the the handlebars_nba_scrape DB
-router.get("/api/scrape", function(req,res) {
+// run through postman to populate the the handlebars_nba_scrape DB
+router.get("/api/scrape", function(req, res) {
 	request('http://www.espn.com/nba/statistics/team/_/stat/team-comparison-per-game', function(err, response, html) {
 		if (err) {
 			throw err;
 		}
 		var $ = cheerio.load(html);
-		var results = [];
+		var results = []; // change to {} ??
     // look for the containing div that holds the entire table you want to scrape.
-    // $('.mod-table') in this case. Find this in the console => elements
-		$('.mod-table').each(function(index, element) {
+    // $('.tablehead') in this case. Find this in the console => elements
+		$('.tablehead').each(function(index, element) {
 			// console.log(index);
-			var table = $(element).find("table");
-			table.each(function() {
+			// console.log(element);
+			// var table = $(element).find("table");
+			// table.each(function() {
 				var tr = $(this).find("tr");
 				tr.each(function() {
+					var statsCounter = 0; // is this in the right place?
 					var team = $(this).find("td").eq(1).text().trim();
 					// console.log(team);
 					var ppg = $(this).find("td").eq(2).text().trim();
 					// console.log(ppg);
-          // this conditional tests is fields have letters...
+          // this conditional tests if fields have letters...
           // you can cl it. console.log(ppg.match(/[A-Za-z]/) == null);
 					if (ppg.match(/[A-Za-z]/) == null) { // eslint-disable-line
 						results.push({ team: team, ppg: parseFloat(ppg) });
 						// console.log(ppg.match(/[A-Za-z]/) == null);
+						// tr.append(team).append(ppg); ????
 					}
 				});
-			});
+			// });
 		});
+		res.json(results);
     // console.log(results); // in order to see this CL you need to run through postman, will return the srape in the terminal!
-		models.Stat.bulkCreate(results);
-		// res.json(results);
+		// models.Stat.bulkCreate(results);
 	});
 });
 module.exports = router;
